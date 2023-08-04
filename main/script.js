d7b19fee19e10eefd7c0a0a8028ed8679122b248
Harshit Awasthi <awasthiharshit8@gmail.com>
2023-08-04 18:03:36 +0530

    third commit

--- main/script.js
+++ main/script.js
@@ -1,8 +1,18 @@
-
-
 let value1 = document.getElementById('value1');
 let value2 = document.getElementById('value2');
 let value3 = document.getElementById('value3');
+let won = document.getElementById('win');
+let loose = document.getElementById('lose');
+
+let text1 = '<div class="alert alert-success" role="alert" style="display: inline-block;">' +
+            '<strong>Congrats!!! You Won!!!</strong>' +
+            '</div>';
+
+let text2 = '<div class="alert alert-warning" role="alert" style="background-color: lightyellow; color: red;display: inline-block;">' +
+            '<strong>Better Luck Next Time !!!<br>' +
+            'You Lose</strong>' +
+            '</div>';
+
 let startButton = document.getElementById('startButton');
 
 let values = ['😇', '🥶', '🤯', '🥳', '😓', '🤠', '🤩', '😵‍💫', '🤑'];
@@ -26,33 +36,31 @@ function startAnimation() {
   value1.style.animation = 'none';
   value2.style.animation = 'none';
   value3.style.animation = 'none';
-  function calculate()
-  {
 
-    let count=0;
+  function calculate() {
+    let count = 0;
 
-    if(value1.innerText == '😓' || value1.innerText == '😵‍💫' || value1.innerText == '🤯' || value1.innerText == '🥶' )
-    {
+    if (['😓', '😵‍💫', '🤯', '🥶'].includes(value1.innerText)) {
       count++;
     }
-    if(value2.innerText == '😓' || value2.innerText == '😵‍💫' || value2.innerText == '🤯' || value2.innerText == '🥶' )
-    {
+    if (['😓', '😵‍💫', '🤯', '🥶'].includes(value2.innerText)) {
       count++;
     }
-    if(value3.innerText == '😓' || value3.innerText == '😵‍💫' || value3.innerText == '🤯' || value3.innerText == '🥶' )
-    {
+    if (['😓', '😵‍💫', '🤯', '🥶'].includes(value3.innerText)) {
       count++;
     }
-    if(count<=1)
-    {
+
+    if (count <= 1) {
       console.log("winner");
-      
-    }
-    else{
+      let add = document.createElement('div');
+      add.innerHTML = text1;
+      won.appendChild(add);
+    } else {
       console.log("loser");
+      let add = document.createElement('div');
+      add.innerHTML = text2;
+      won.appendChild(add);
     }
-
-
   }
 
   setTimeout(() => {
@@ -76,16 +84,3 @@ function startAnimation() {
 }
 
 startButton.addEventListener('click', startAnimation);
-
-
-
-
-
-
-
-
-
-
-
-
-
